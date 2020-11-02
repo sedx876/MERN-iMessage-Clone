0e842f4f97552c624aff206b6b2bcc3e8dbd142f
sedx876 <swatkins876@gmail.com>
2020-11-02 05:55:50 -0500

    updated app.js and index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
-import './styles/App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./style/App.css";
+import { selectUser, login, logout } from "./features/userSlice";
+import Imessage from "./Imessage";
+import Login from "./Login";
+import { auth } from "./firebase";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user is logged in
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        // user is logged out
+        dispatch(logout());
+      }
+    });
+  }, []);
+
+  return <div className="app">{user ? <Imessage /> : <Login />}</div>;
 }
 
 export default App;
